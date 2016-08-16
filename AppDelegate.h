79a8e1e7d021785fc5dac6e3f65525fd11d33364
silver6wings <silver6wings@gmail.com>
2016-08-17 00:04:02 +0800

    add project

---
+++ AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  kaizalatest
+//
+//  Created by silver6wings on 16/8/16.
+//  Copyright © 2016年 silver6wings. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  kaizalatest
+//
+//  Created by silver6wings on 16/8/16.
+//  Copyright © 2016年 silver6wings. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
